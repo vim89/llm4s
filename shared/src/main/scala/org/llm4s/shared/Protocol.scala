ed20b72b91a81fb0faa66b93c59218815c0dc72b
Rory Graves <rory.graves@thetradedesk.com>
2025-02-28 07:20:19 +0000

    feat: Add request and response models for directory listing commands

---
+++ shared/src/main/scala/org/llm4s/shared/Protocol.scala
@@ -0,0 +1,24 @@
+package org.llm4s.shared
+
+import upickle.default.{ReadWriter, macroRW}
+
+sealed trait Request
+
+case class ListDirectoryCommand(commandId: String, path: String) extends Request
+
+object ListDirectoryCommand {
+  implicit val rw: ReadWriter[ListDirectoryCommand] = macroRW
+}
+
+sealed trait Response
+case class ListDirectoryResponse(commandId: String, files: List[String]) extends Response
+
+object ListDirectoryResponse {
+  implicit val rw: ReadWriter[ListDirectoryResponse] = macroRW
+}
+
+case class ErrorResponse(commandId: String, errorMessage: String) extends Response
+
+object ErrorResponse {
+  implicit val rw: ReadWriter[ErrorResponse] = macroRW
+}
