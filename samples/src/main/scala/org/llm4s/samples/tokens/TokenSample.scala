00750ffd0ef46b03a549ec2a4e9144aecc0ba342
Dmitry Mamonov <dmitry.s.mamonov@gmail.com>
2025-03-26 19:09:41 +0000

    basic tokens functionality / attempt 4

---
+++ samples/src/main/scala/org/llm4s/samples/tokens/TokenSample.scala
@@ -0,0 +1,15 @@
+package org.llm4s.samples.tokens
+
+import org.llm4s.identity.TokenizerId
+import org.llm4s.identity.TokenizerId.{ CL100K_BASE, O200K_BASE, R50K_BASE }
+import org.llm4s.tokens.Tokenizer
+
+object TokenSample {
+  def main(args: Array[String]): Unit =
+    for (tokenizerId <- List(R50K_BASE, CL100K_BASE, O200K_BASE)) {
+      val tokenizer = Tokenizer.lookupStringTokenizer(tokenizerId).get
+      val message = "Hello Scala!"
+      val tokens = tokenizer.encode(message)
+      println(s"`$message` / $tokenizerId -> $tokens")
+    }
+}
