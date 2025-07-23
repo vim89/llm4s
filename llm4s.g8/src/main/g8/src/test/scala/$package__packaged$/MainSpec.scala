564eb3f0e1f7744f57eee4436c5a0e05d8b99ddf
Vitthal Mirji <vitthalmirji@gmail.com>
2025-07-23 05:49:04 +0530

    refactored for claude review

--- llm4s.g8/src/main/g8/src/test/scala/$package__packaged$/MainSpec.scala
+++ llm4s.g8/src/main/g8/src/test/scala/$package__packaged$/MainSpec.scala
@@ -1,8 +1,12 @@
-package org.llm4s.template
+package $package;format="package"$
 
 /**
  * This code is part of the Giter8 template llm4s.g8 in llm4s project, which provides a set standard template/archetype
  * for improve developer onboarding, creating new projects using the llm4s library.
+ */
+
+/** The MainSpec class contains unit tests for the PromptExecutor functionality.
+ * It checks basic assertions and the response from the LLM when a prompt is executed.
  */
 class MainSpec extends munit.FunSuite {
   test("basic assertion") {
--- llm4s.g8/src/main/g8/src/test/scala/$package__packaged$/PromptExecutorSpec.scala
+++ llm4s.g8/src/main/g8/src/test/scala/$package__packaged$/PromptExecutorSpec.scala
@@ -4,6 +4,11 @@ package $package; format = "package" $
  * This code is part of the Giter8 template llm4s.g8 in llm4s project, which provides a set standard template/archetype
  * for improve developer onboarding, creating new projects using the llm4s library.
  */
+
+/** The PromptExecutorSpec class contains unit tests for the PromptExecutor functionality.
+ * It checks basic assertions and the response from the LLM when a prompt is executed.
+ */
+
 class PromptExecutorSpec extends munit.FunSuite {
   test("basic assertion") {
     assert(1 + 1 == 2)
